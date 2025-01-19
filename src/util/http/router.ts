12307cc6c80bb52356c721ec90222c31e72c50f7
Arnav Deo <arnav.nitin.deo@gmail.com>
2025-01-19 15:42:03 +0530

    fix: 🐛 Fix incorrect Router build error

--- src/util/http/router.ts
+++ src/util/http/router.ts
@@ -1,4 +1,6 @@
 import { Router as ExpressRouter } from "express"
 
 /** Extends express.Router to make `mergeParams: true` by default */
-export const Router = ExpressRouter.bind(ExpressRouter, { mergeParams: true })
+export function Router() {
+	return ExpressRouter({ mergeParams: true })
+}
